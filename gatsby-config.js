adff0a19899db474bd0176fb43e394a83ff0264e
Sroop Sunar <sroop@peg.co>
2020-05-12 17:03:01 +0100

    Install Contentful and Styled Components

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: `preview.contentful.com`,
+      },
+    },
+  ]
 }
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
+import styled from "styled-components"
 
-export default () => <div>Hello world!</div>
+export default () => <Message>Hello world!</Message>
+
+const Message = styled.div`
+  color: red;
+`
